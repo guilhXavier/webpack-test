3c725689bb4aacd9839c6d10f5f113f4741f1e2b
guilhXavier <guilhermetamara5@gmail.com>
2021-03-07 21:12:19 -0300

    initial

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function component() {
+	const elem = document.createElement('div')
+
+	elem.innerHTML = _.join(['Hello', 'Webpack!'], ' ')
+
+	return elem
+}
+
+document.body.appendChild(component())
