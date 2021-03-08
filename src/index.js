ec45c1fdada65a5c690db3c1f85e31406632908b
guilhXavier <guilhermetamara5@gmail.com>
2021-03-07 21:47:11 -0300

    finalizando

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import _ from 'lodash'
+import './style.css'
+import Icon from './download.jpeg'
+import Data from './data.xml'
+import Notes from './data.csv'
 
 function component() {
 	const elem = document.createElement('div')
 
 	elem.innerHTML = _.join(['Hello', 'Webpack!'], ' ')
+	elem.classList.add('hello')
+
+	const myIcon = new Image()
+	myIcon.src = Icon
+
+	elem.appendChild(myIcon)
+
+	console.log(Data, 1)
+	console.log(Notes, 2)
 
 	return elem
 }
